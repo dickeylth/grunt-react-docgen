667c49b78818bbacc3bd6904fad2623d16df2446
弘树 <tiehang.lth@taobao.com>
2015-12-24 20:16:45 +0800

    init version of 0.1.0

---
+++ README.md
@@ -0,0 +1,91 @@
+# grunt-react-docgen
+
+> Grunt plugin based on react-docgen and doctrine for react component doc json generate.
+
+## 快速上手
+
+### 安装
+
+```shell
+npm install grunt-react-docgen --save-dev
+```
+
+### Gruntfile.js
+
+```js
+grunt.loadNpmTasks('grunt-react-docgen');
+```
+
+## 任务配置
+
+### 概览
+
+```js
+grunt.initConfig({
+  react_docgen: {
+    options: {
+      // Task-specific options go here.
+    },
+    your_target: {
+      // Target-specific file lists and/or options go here.
+    },
+  },
+})
+```
+
+### Options
+
+#### options.entryFiles
+Type: `Array`
+Default value: `['index.jsx']`
+
+需要生成自动化文档的入口文件列表。
+
+#### options.pkgInfo
+Type: `Object`
+Default value: `require(path.join(process.cwd(), 'package.json'))`
+
+组件基本信息，默认取 `package.json`。
+
+#### options.demoEntryJSX
+Type: `String`
+Default value: `'demo/index.jsx'`
+
+组件 Demo 入口的 JSX 代码文件。
+
+#### options.demoEntryHTML
+Type: `String`
+Default value: `'demo/index.html'`
+
+组件 Demo 入口的 HTML 代码文件。
+
+#### options.demoBuild
+Type: `String`
+Default value: `'demo.js'`
+
+组件 Demo 使用的构建最终的 js 文件。
+
+
+#### options.demoContentSelector
+Type: `String`
+Default value: `'#J_Page'`
+
+组件 Demo 入口的 HTML 中示例所在的 DOM 节点。
+
+#### options.outputFilePath
+Type: `String`
+Default value: `'build/doc.html'`
+
+输出文档 HTML 文件路径。
+
+## 版本历史
+
+- [0.1.0]
+    - 初始记录
+
+## License
+Copyright (c) 2015 dickeylth. Licensed under the MIT license.
+
+## Credits
+
+- Inspired by [stardust](https://github.com/TechnologyAdvice/stardust)'s [gulp-react-docgen.js](https://github.com/TechnologyAdvice/stardust/blob/master/gulp/plugins/gulp-react-docgen.js)
---
+++ tasks/react_docgen.js
@@ -0,0 +1,213 @@
+/*
+ * grunt-react-docgen
+ * https://github.com/dickeylth/grunt-react-docgen
+ *
+ * Copyright (c) 2015 dickeylth
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+var reactDocgen = require('react-docgen');
+var doctrine = require('doctrine');
+var Juicer = require('juicer');
+var cheerio = require('cheerio');
+var _ = require('lodash')
+var fs = require('fs');
+var path = require('path');
+
+const DOC_TPL_SOURCE = fs.readFileSync(path.join(__dirname, './doc.html'), 'utf-8');
+
+/**
+ * 生成源码
+ * @param filePath {String} 文件路径
+ */
+function genCodeSource(filePath) {
+  return fs.readFileSync(filePath, 'utf-8').trim().replace(/[\u00A0-\u9999<>\&]/gim, function(i) {
+    return '&#'+i.charCodeAt(0)+';';
+  });
+}
+
+function parseDocBlock(docBlock) {
+    return doctrine.parse(docBlock || '', {unwrap: true});
+}
+
+function extractHTMLBySelector(htmlPath, selector) {
+  var htmlSource = fs.readFileSync(htmlPath, 'utf-8');
+  var $ = cheerio.load(htmlSource);
+  return $(selector).html();
+}
+
+/**
+ * [
+  {
+    "description": "@class Switch\n@extends React.Component\n@desc Switch Component for mobile",
+    "props": {
+      "defaultChecked": {
+        "type": {
+          "name": "bool"
+        },
+        "required": false,
+        "description": "是否选中",
+        "defaultValue": {
+          "value": "false",
+          "computed": false
+        },
+        "docBlock": {
+          "description": "是否选中",
+          "tags": []
+        }
+      },
+      "onChange": {
+        "type": {
+          "name": "func"
+        },
+        "required": false,
+        "description": "选中状态变更回调\n@param {Boolean} checked 是否选中",
+        "docBlock": {
+          "description": "选中状态变更回调",
+          "tags": [
+            {
+              "title": "param",
+              "description": "是否选中",
+              "type": {
+                "type": "NameExpression",
+                "name": "Boolean"
+              },
+              "name": "checked"
+            }
+          ]
+        }
+      },
+      "disabled": {
+        "type": {
+          "name": "bool"
+        },
+        "required": false,
+        "description": "是否禁用",
+        "defaultValue": {
+          "value": "false",
+          "computed": false
+        },
+        "docBlock": {
+          "description": "是否禁用",
+          "tags": []
+        }
+      }
+    },
+    "docBlock": {
+      "description": "",
+      "tags": [
+        {
+          "title": "class",
+          "description": null,
+          "type": null,
+          "name": "Switch"
+        },
+        {
+          "title": "extends",
+          "description": null,
+          "type": null,
+          "name": "React.Component"
+        },
+        {
+          "title": "desc",
+          "description": "Switch Component for mobile"
+        }
+      ]
+    }
+  }
+] */
+function reFormatFileDocMetaArr (fileDocMetaArr) {
+  return fileDocMetaArr.map(function (fileDocItem) {
+    var docBlock = fileDocItem.docBlock;
+    var classMetaTags = docBlock.tags;
+    var className = _.capitalize(fileDocItem.fileName);
+    var classMetaInfo = {
+      name: className,
+      extend: 'React.Component',
+      desc: 'React Component ' + className
+    };
+    classMetaTags.forEach(function(metaTag){
+      if(metaTag.title == 'class') {
+        classMetaInfo.name = metaTag.name || classMetaInfo.name;
+      } else if(metaTag.title == 'extend') {
+        classMetaInfo.extend = metaTag.name || classMetaInfo.extend;
+      } else if(metaTag.title == 'desc' || metaTag.title == 'description') {
+        classMetaInfo.desc = metaTag.description || classMetaInfo.desc;
+      }
+    });
+    classMetaInfo.props = fileDocItem.props;
+    return classMetaInfo;
+  });
+}
+
+module.exports = function (grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('react_docgen', 'Grunt plugin based on react-docgen and doctrine for react component doc json generate', function () {
+
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      entryFiles: ['index.jsx'],
+      pkgInfo: require(path.join(process.cwd(), 'package.json')),
+      demoEntryJSX: 'demo/index.jsx',
+      demoEntryHTML: 'demo/index.html',
+      demoBuild: 'demo.js',
+      outputFilePath: 'build/doc.html',
+      demoContentSelector: '#J_Page'
+    });
+
+    var fileDocMetaArr = [];
+
+    // Iterate over all specified file groups.
+    options.entryFiles.forEach(function (filepath) {
+
+      if (!grunt.file.exists(filepath)) {
+        grunt.log.warn('Source file "' + filepath + '" not found.');
+      } else {
+        var src = grunt.file.read(filepath);
+        var parsed = reactDocgen.parse(src);
+
+        // replace the component`description` string with a parsed doc block object
+        parsed.docBlock = parseDocBlock(parsed.description);
+
+        // replace prop `description` strings with a parsed doc block object
+        var props = [];
+        _.each(parsed.props, function(propDef, propName) {
+          props.push(_.merge(propDef, {
+            name: propName,
+            docBlock: parseDocBlock(propDef.description)
+          }));
+        });
+        parsed.props = props;
+        parsed.fileName = path.basename(filepath).split('.')[0];
+
+        fileDocMetaArr.push(parsed);
+      }
+    });
+
+    var pkgInfo = options.pkgInfo;
+    Juicer.register('json', function (data) {
+      return JSON.stringify(data, null, 2);
+    });
+    Juicer.register('lowercase', function (str) {
+      return str.toLowerCase();
+    });
+    grunt.file.write(options.outputFilePath, Juicer(DOC_TPL_SOURCE, {
+      name: pkgInfo.name,
+      description: pkgInfo.description,
+      author: Array.isArray(pkgInfo.author) ? pkgInfo.author.join('|') : pkgInfo.author,
+      demoBuild: options.demoBuild,
+      demoContent: extractHTMLBySelector(options.demoEntryHTML, options.demoContentSelector),
+      sourceCode: genCodeSource(options.demoEntryJSX),
+      apiDetailArr: reFormatFileDocMetaArr(fileDocMetaArr)
+    }));
+
+    // Print a success message.
+    grunt.log.writeln('Code document generated in ' + options.outputFilePath + '.');
+
+  });
+
+};
